f3f81287c7c311e95ff27c3f41512f144831baf4
TATY <tatyccsilva@gmail.com>
2019-06-26 14:26:15 +0200

    Create a model Team

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
-const sequelize = new Sequelize(databaseUrl)
+const sequelize = new Sequelize(databaseUrl, { define: { timestamps: false } })
 
 sequelize
     .sync()
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
-const sequelize = require('./db')
+const db = require('./db')
+const Team = require('./team/model')
 
 app.listen(port, function () {
     console.log(`Web server listening on port :${port}`)
