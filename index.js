66eaa36954332ac0b24af200d8fa80071cda6d97
TATY <tatyccsilva@gmail.com>
2019-06-26 14:02:55 +0200

    Create db.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+const sequelize = require('./db')
 
 app.listen(port, function () {
     console.log(`Web server listening on port :${port}`)
