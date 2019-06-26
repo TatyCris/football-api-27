ab995287d42ea5446dad96fb3df81576a25a6cff
TATY <tatyccsilva@gmail.com>
2019-06-26 16:39:22 +0200

    REST

--- team/router.js
+++ team/router.js
@@ -4,7 +4,7 @@ const Team = require('./model')
 
 router.get('/team', function (req, res, next) {
     Team.findAll()
-        .then(teams => res.json({teams: teams}))
+        .then(teams => res.json({ teams: teams }))
         .catch(err => next(err))
 })
 
@@ -12,6 +12,26 @@ router.post('/team', function (req, res, next) {
     Team
         .create(req.body)
         .then(teams => res.json(teams))
+        .catch(err => next(err))
+})
+
+router.get('/team/:id', function (req, res, next) {
+    const id = req.params.id
+    Team.findByPk(id)
+        .then(teams => res.json(teams))
+        .catch(err => next(err))
+})
+
+router.put('/team/:id', function (req, res, next) {
+    const id = req.params.id
+    // const { id } = req.params
+    const name = req.body.name
+    // const { name } = req.body
+    Team.findByPk(id)
+        .then(team => {
+            return team.update({ name })
+        })
+        .then(team => res.json(team))
         .catch(err => next(err))
 })
 
